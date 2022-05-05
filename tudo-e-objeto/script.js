caa89dc681ed0afc4147108d784e4e2ce7c0f3ac
Thais <thaisuckeman@yahoo.com.br>
2022-05-05 14:27:05 -0300

    objeto

---
+++ tudo-e-objeto/script.js
@@ -0,0 +1,27 @@
+var nome = 'Thaís';
+var nomeMinusculo = nome.toLocaleLowerCase();
+
+var btn = document.querySelector('.btn')
+
+
+// nomeie 3 propriedades ou métodos de strings
+var nome = 'Thaís';
+//fixed cria um elemento HTML <tt> que faz com que uma string seja exibida em uma fonte de densidade fixa.
+//length conta o  numero de aqgumentos que a função  possui 
+//slice  retorna uma cópia de parte de um array a partir de um subarray criado entre as posições início e fim (fim não é necessário) de um array original. O Array original não é modificado.
+
+
+
+// nomeie 5 propriedades ou métodos de elemenntos do DOM
+var btn = document.querySelector('.btn');
+// addEventlistener
+//appendChild
+//id
+//innerHtml
+btn.
+  console.log(btn)
+
+
+
+// BUSQUE  na web um objeto (método) capaz de interagir com o clipbord.
+//clipbord é a parte do seu computador que lida com ctrl+ c ctrl+v
