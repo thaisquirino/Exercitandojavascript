088c95b16a2b1f1f25c6eead090d3c632af2ee18
Thais <thaisuckeman@yahoo.com.br>
2022-05-09 16:25:49 -0300

    arrays e loops

---
+++ arrays-e-loops/scripts.js
@@ -0,0 +1,15 @@
+var videoGames = ['switch', 'Ps4', 'Xbox'];
+videoGames.push('3DS')
+
+
+var ultimoItem = videoGames.pop();
+
+for (var numero = 1; numero <= 100; numero++) {
+  console.log(numero);
+}
+
+var i = 0;
+while (i <= 100) {
+  console.log(i);
+  i = i + 5;
+}
