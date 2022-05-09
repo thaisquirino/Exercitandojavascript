ef47b9be32ea800f9b647928fe714806e056a833
Thais <thaisuckeman@yahoo.com.br>
2022-05-09 18:48:57 -0300

    exercicios

--- atribuicaoeternario/script.js
+++ atribuicaoeternario/script.js
@@ -16,3 +16,22 @@ if (possuiFaculdade)
   console.log('sim possui');
 else
   console.log('Não possui')
+
+
+//some  500 ao valor de scrool abaixo,
+// atribuindo o novo valor a scroll
+var scrool = 1000;
+scroll += 500;
+console.log(scroll);
+
+
+// Atribua true para a variável darCredito,
+// caso o clintew possua carro e casa.
+// E false caso o contrário.
+var possuiCarro = true;
+var possuiCasa = true;
+var darCredito;
+
+darCredito = (possuiCarro && possuiCasa);
+console.log(darCredito) ? 'Dar crédito ao cliente' : 'Negar crédito';
+console.log(darCredito)
