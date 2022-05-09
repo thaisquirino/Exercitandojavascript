a4dc4fb7d47956a5b042fd5743d82a6f525af1b5
Thais <thaisuckeman@yahoo.com.br>
2022-05-09 18:38:16 -0300

    Atribuição e ternario

---
+++ atribuicaoeternario/script.js
@@ -0,0 +1,18 @@
+var numero = 20;
+
+numero += 10 // numero
+console.log(numero);
+
+var idade = 20;
+var naoPossuiDiabetes = false;
+
+var podeBeber;
+podeBeber = (idade >= 18 && naoPossuiDiabetes) ? 'Pode beber' : 'Não pode beber';
+console.log(podeBeber)
+
+
+var possuiFaculdade = false;
+if (possuiFaculdade)
+  console.log('sim possui');
+else
+  console.log('Não possui')
