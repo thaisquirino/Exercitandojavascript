73052dc195ba0dfa79c85c0e7bf34c153f2f2555
Thais Quirino <thais.iblue@ciclic.com.br>
2022-09-11 12:29:27 -0300

    event2

--- eventos/script.js
+++ eventos/script.js
@@ -1,7 +1,40 @@
-const img = document.querySelector('img');
- img.addEventListener('click', function () {
-    console.log('clicou')
- })
- 
+const linkExterno = document.querySelector('a[href^="http"]');
 
-console.log(img)
+function handleLinkExterno (event) {
+    event.preventDefault();
+    console.log('event');
+}
+linkExterno.addEventListener('click',handleLinkExterno);
+
+const h1 = document.querySelector('h1');
+
+function handleEvent(event){
+    console.log(event.type,event);
+}
+
+// h1.addEventListener('click',handleEvent);
+// h1.addEventListener('mouseenter',handleEvent);
+// h1.addEventListener('mousemove',handleEvent);
+
+// window.addEventListener ('scroll',handleEvent);
+// window.addEventListener('resize',handleEvent);
+// window.addEventListener('keydown',handleEvent);
+
+function handleKeyboard (event) {
+    if(event.key === 'a'){
+        document.body.classList.toggle('azul')
+    }
+}
+
+window.addEventListener('keydown',handleKeyboard);
+const imgs = document.querySelectorAll('img');
+
+function handleImg(event){
+    console.log(event.target.getAttribute('src'));
+}
+
+imgs.forEach((img) => {
+    img.addEventListener('click',handleImg);
+});
+
+ 
