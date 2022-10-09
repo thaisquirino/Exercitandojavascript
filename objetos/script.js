32ecef89836c57192349345d50b85633e39bcaf3
Thais Quirino <thais.iblue@ciclic.com.br>
2022-10-09 11:29:50 -0300

    Constructor Function 2

--- objetos/script.js
+++ objetos/script.js
@@ -1,26 +1,50 @@
-// const Dom = {
-//  seletor:'li',
-//  element(){
-//     return elementoSelecionado = document.querySelector (this.seletor);
-//  },
-//  ativar(){
-//     const elementoSelecionado = this.element();
-//     elementoSelecionado.classList.add('ativar');
-//  }
-// }
+//Transforme o objeto abaixo em uma Constructor Function
+function Carro () {
+    this.marca='Marca';
+    this.preco = 2000;
+   }
+   const honda = new Carro();
 
-
-function Dom( seletor) {
-     this.element =function() {
-        return document.querySelector(this.seletor);
+ function Pessoa (nome,idade){
+    this.nome = nome;
+    this.idade = idade;
+    this.andar = function () {
+        console.log(this.nome + 'Andou')
     }
-   this.ativar=function(){
-        this.element().classList.add('ativar');
+ }
+
+   
+   //Crie 3 pessoas, Thaís -28 anos,
+   //Maria -60 ,Fernando - 34anos,
+
+    const Thais = new Pessoa ('Thais',28);
+    const Maria = new Pessoa ('Maria',60);
+    const Fernando = new Pessoa ('Fernando',34);
+   
+   //Crie uma Constructor Function (Dom) para manipulação
+   //de listas de elementos do  dom. Deve conter as seguintes
+   // propriedades e métodos:
+   //
+   // elements, retorna NodeList com os elementos selecionados
+   // addClass(classe), adiciona a classe a todos os elementos
+   // removeClass(classe), remove a classe a todos os elementos
+
+   function Dom (seletor) {
+    const elementList = document.querySelectorAll(seletor);
+    this.elements=elementList;
+    this.addClass = function (classe) {
+     elementList.forEach((element) =>{
+      element.classList.add(classe);
+     })
     }
-}
+    this.removeClass = function (classe) {
+        elementList.forEach((element) =>{
+         element.classList.remove(classe);
+        })
+      }
+   }
+   const listaItems = new Dom ('li'); 
+   const ul = new Dom ('ul');
 
-const li = new Dom ('li');
-const ul = new Dom ('ul');
-
-const lastLi =new Dom ('li:last-child');
-lastLi.ativar();
+//    listaItens.addClass('ativar');
+//    ul.addClass('ativar-ul')
--- objetos/scriptaula-1.js
+++
@@ -1,5 +0,0 @@
-function Carro () {
- this.marca='Marca';
- this.preco = 2000;
-}
-const honda = new Carro();
