bcac6b4fa2b0aa02b9f8e5bd2bcd279c8eafd724
Thais Quirino <thais.iblue@ciclic.com.br>
2022-10-12 19:53:16 -0300

    strings

--- Native,Host e User/script.js
+++ Native,Host e User/script.js
@@ -1,35 +1,86 @@
-// Crie uma função construtora de Pessoas
-// Deve conter nome, sobrenome e idade
-// Crie um método no protótipo que retorne
-// o nome completo da pessoa
-  function Pessoa(nome,sobrenome,idade) {
-     this.nome = 'nome';
-     this.sobrenome = 'sobrenome';
-     this.idade = idade;
- }
- Pessoa.prototype.nomeCompleto =function(){
-  return `${this.nome}` `${this.sobrenome}`
- }
-const andre = new Pessoa ('Thais','Quirino',28);
+// const  comida ='Pizza';
 
-// Liste os métodos acessados por 
-// dados criados com NodeList,
-// HTMLCollection, Document
-Object.getOwnPropertyNames(NodeList.prototype)
-Object.getOwnPropertyNames(HTMLCollection.prototype)
-Object.getOwnPropertyNames(Document.prototype)
+// const agua = new String('Agua');
 
 
-// Liste os construtores dos dados abaixo
-const li = document.querySelector('li');
+// console.log(agua.length);
 
-li;//HTMLLIElement
-li.click;//Function
-li.innerText;//String
-li.value;//Number
-li.hidden;//Boolean
-li.offsetLeft;// Number
-li.click();
+// const frase = 'A melhor comida';
 
-// Qual o construtor do dado abaixo:
-li.hidden.constructor.name;
+// console.log(frase.charAt(frase.length))
+
+
+// console.log(frase[2])
+
+
+const frase = 'A melhor  linguagem  é';
+const linguagem = 'JavaScript';
+
+
+const fraseFinal = frase + linguagem ;
+
+
+const fruta='Banana';
+const listaFrutas = 'Melancia,Banana,Laranja';
+
+
+// console.log(listaFrutas.includes(fruta));
+
+console.log(fruta.startsWith('Banana'));
+
+
+const transacao1 = 'Depósito de cliente';
+const transacao2 = 'Depósito de fornecedor';
+const transacao3 = 'Taxa de camisas';
+
+
+console.log(transacao1.slice(5));
+
+transacao1.slice(0, 3); // Dep
+transacao2.slice(0, 3); // Dep
+transacao3.slice(0, 3); // Tax
+
+transacao1.slice(12); // cliente
+transacao1.slice(-4); // ente
+transacao1.slice(3, 6); // ósi
+
+
+console.log (fruta.indexOf('a'));
+
+console.log (fruta.lastIndexOf('na'));
+
+
+
+const listaPrecos = ['R$ 99', 'R$ 199', 'R$ 12000'];
+
+listaPrecos.forEach((preco) => {
+  console.log(preco.padStart(10, '.'));
+})
+
+listaPrecos[0].padStart(10, '.'); // .....R$ 99
+listaPrecos[0].padEnd(10, '.');   // R$ 99.....
+
+
+let listaItens = 'Camisas Bonés Calças Bermudas Vestidos Saias';
+listaItens = listaItens.replace(/[ ]+/g, ', ');
+
+const listaItens = 'Camisas Bonés Calças Bermudas Vestidos Saias';
+const arrayItens = listaItens.split(' ');
+
+const htmlText = '<div>O melhor item</div><div>A melhor lista</div>';
+const htmlArray = htmlText.split('div');
+const htmlNovo = htmlArray.join('section');
+
+
+const sexo1 = 'Feminino';
+const sexo2 = 'feminino';
+const sexo3 = 'FEMININO';
+
+(sexo1.toLowerCase() === 'feminino'); // true
+(sexo2.toLowerCase() === 'feminino'); // true
+(sexo3.toLowerCase() === 'feminino'); // true
+
+const valor = '  R$ 23.00   ' 
+valor.trim(); // 'R$ 23.00'
+valor.trimStart(); // 'R$ 23.00   '
+valor.trimEnd(); // '  R$ 23.00'
