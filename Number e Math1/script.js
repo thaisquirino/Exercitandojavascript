5361387ae247023cdf54fa2baf58dcf58075b2c6
Thais Quirino <thais.iblue@ciclic.com.br>
2022-10-17 14:13:50 -0300

    Number e Math2

---
+++ Number e Math1/script.js
@@ -0,0 +1,26 @@
+// console.log(Number.isNaN('ds'));
+// console.log(Number.isInteger(10.1459666));
+
+// console.log(parseFloat('324334,8953'))
+// console.log(parseFloat('100.27 reais'))
+// console.log(parseInt('100.27 reias'))
+// console.log(parseInt(23.49))
+
+//  const preco =10.2566999;
+//  console.log (+preco.toFixed())
+
+//  let valor =48.49;
+//  valor = valor.toLocaleString('pt-BR',{style:'currency', currency:'BRL'});
+
+//  console.log(valor)
+//  console.log(Math.PI)
+
+
+//  const aleatorio =Math.floor(Math.random()*10);
+
+//  console.log(aleatorio)
+
+
+
+
+
