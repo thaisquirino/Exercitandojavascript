99b6bf18bc03a4a77a3d586b601356ce2b1660f0
Thais Quirino <thais.iblue@ciclic.com.br>
2022-11-02 13:50:13 -0300

    Object exercitando

--- Object/script.js
+++ Object/script.js
@@ -1,92 +1,37 @@
-// const pessoa = new Object({
-// nome:'Thaís'
-// })
-// console.log(pessoa)
 
-// const carro={
-//   rodas:4,
-//   init(valor){
-//     this.marca = valor;
-//     return this;
-//   },
-//   acelerar(){
-//     return this.marca + 'acelerou';
-//   },
-//   buzinar(){
-//     return this.marca + 'buzinou';
-//   }
-// } 
-
-// const honda = Object.create(carro) .init('Honda');
-
-// console.log(honda.acelerar());
-
-// const ferrari = Object.create(carro).init('Ferrari');
-
-// console.log(ferrari.acelerar());
-
-// const funcaoAutomovel ={
-//   acelerar() {
-//     return 'acelerou';
-//   },
-//   buzinar(){
-//     return 'buzinou';
-//   },
-// }
-
-// const moto ={
-//   rodas:2,
-//   capacete:true,
-// }
-// Object.assign(moto,funcaoAutomovel);
-
-// console.log(moto)
-
-// const moto ={}
-
-// console.log(moto)
-
-// Object.defineProperties(moto,{
-//   rodas:{
-//     value:2,
-//     configurable:false,
-
-//   }
-
-
-// })
-
-//  const moto ={
-//   capacete:true,
-//  }
-
-// Object.defineProperties(moto,{
-//   rodas:{
-//     enumerable:true,
-//     get() {
-//       return this._rodas;
-//     },
-//     set(valor){
-//       this.rodas = valor * 4 + 'Total Rodas'
-//     }
-//     }
-// })
-//  console.log (moto)
-
-//  const innerHeightConfig = Object.getOwnPropertyDescriptor(window,'innerHeight');
-
-const frutas =['Banana','Uva'];
-const  frase = 'Oi Frase';
-const somar = function(a,b){
-  return a + b;
-};
- const carro ={
-  marca:'Ford'
+// Crie uma função que verifique
+// corretamente o tipo de dado
+ function verificarDado(dado) {
+  return Object.prototype.toString.call(dado)
  }
 
+console.log(verificarDado([]));
 
-console.log(frase.toString ());
+// Crie um objeto quadrado com
+// a propriedade lados e torne
+// ela imutável
+const quadrado ={};
+Object.defineProperties(quadrado,{
+  lados:{
+    value:4,
+    enumerable:true,
+  }
+})
+console.log(quadrado)
 
-Object.prototype.toString.call(frutas)
 
+// Previna qualquer mudança
+// no objeto abaixo
+const configuracao = {
+  width: 800,
+  height: 600,
+  background: '#333'
+}
+Object.freeze(configuracao);
 
+// Liste o nome de todas
+// as propriedades do
+// protótipo de String e Array
+
+console.log(Object.getOwnPropertyNames(String.prototype))
+console.log(Object.getOwnPropertyNames(Array.prototype))
