8237fc292f1827d143107db234fdcd756f502ceb
Thais Quirino <thaisuckeman@yahoo.com.br>
2022-12-18 08:39:53 -0300

    feat:fetch

---
+++ fetch/script.js
@@ -0,0 +1,65 @@
+// Utilizando a API https://viacep.com.br/ws/${CEP}/json/
+// crie um formulário onde o usuário pode digitar o cep
+// e o endereço completo é retornado ao clicar em buscar
+ 
+const inputCep = document.getElementById('cep');
+const btnCep = document.getElementById('btnCep');
+const resultadoCep =document.querySelector('.resultadoCep');
+
+btnCep.addEventListener('click',handleClick);
+
+function handleClick (event) {
+    event.preventDefault();
+    const cep = inputCep.value;
+    buscaCep(cep);
+}
+
+function buscaCep(cep) {
+  fetch (`https://viacep.com.br/ws/${cep}/json/`)
+  .then(response => response.text())
+  .then(body =>{
+    resultadoCep.innerText = body;
+  })
+}
+
+
+
+// Utilizando a API https://blockchain.info/ticker
+// retorne no DOM o valor de compra da bitcoin and reais.
+// atualize este valor a cada 30s
+
+const btcDisplay = document.querySelector('.btc');
+
+function fetchBtc(){
+  fetch('https://blockchain.info/ticker')
+  .then(response => response.json())
+  .then(btcJson => {
+    console.log(btcJson.BRL.buy);
+    btcDisplay.innerText = ('R$'+  btcJson.BRL.buy).replace('.', ',');
+  })
+}
+ 
+//setInterval(fetchBtc,100);
+
+fetchBtc();
+
+
+// Utilizando a API https://api.chucknorris.io/jokes/random
+// retorne uma piada randomica do chucknorris, toda vez que
+// clicar em próxima
+
+ const btnProxima =document.querySelector('.proxima');
+ const paragrafoPiada = document.querySelector('.piada');
+
+
+function puxarPiada(){
+  fetch ('https://api.chucknorris.io/jokes/random')
+  .then(r => r.json ())
+  .then(piada =>{
+    paragrafoPiada.innerText = piada.value;
+  })
+}
+
+btnProxima.addEventListener('click',puxarPiada);
+
+puxarPiada();
